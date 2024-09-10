ca8df4fdcfc382b6a2d8f18ac450f14605646c3b
terry.walsworth.4 <terry.walsworth.4@gmail.com>
2024-09-10 15:33:34 -0600

    exe

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
 
 async fn index() -> impl Responder {
     "Hello world!"
